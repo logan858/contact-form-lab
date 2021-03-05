aefb97ab8bbe09341fb3bc960b64b0c4664bb398
Logan <mlogan858@hotmail.com>
2021-03-05 10:45:35 -0500

    .

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 let methodOverride = require("method-override")
+let nodeMail = require("nodemailer")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
